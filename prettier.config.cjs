2fa9aadd36a40f5cef6f271de359f9b08b117539
Omochice <mochice.mls.ntl@gmail.com>
2024-03-27 23:41:31 +0900

    feat: add poc

---
+++ prettier.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  printWidth: 80,
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  plugins: ['prettier-plugin-astro'],
+};
